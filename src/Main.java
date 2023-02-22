67014550fbfe64ffed1e1f6008e345d8f649bfe5
Thomas Busch <b.reinda.2004@gmail.com>
2023-02-22 13:12:13 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,0};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
